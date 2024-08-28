0c2770b4e372e9193ae9f5614b0bf2c8d470c8bf
koog7 <k.ognev709@mail.com>
2024-08-28 14:41:43 +0600

    Написан метод post с генерацией коротких id

--- api/routers/ShortUrlRouter.ts
+++ api/routers/ShortUrlRouter.ts
@@ -1,10 +1,35 @@
 import express from 'express';
+import ShortUrl from "../models/Url";
 
 const ShortUrlRouter = express.Router();
 ShortUrlRouter.use(express.json());
 
-ShortUrlRouter.get('/', async (req, res) => {
+ShortUrlRouter.post('/links', async (req, res) => {
+    const {url} = req.body;
 
+    if(!url){
+        return res.status(400).send('Provide link')
+    }
+
+    const charactersAndDigits = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    try{
+        const split = charactersAndDigits.split('')
+        let shortId = '';
+
+        for(let i = 0; i < 7; i++){
+            shortId += split[Math.floor(Math.random() * split.length)]
+        }
+
+        const UrlObject = new ShortUrl({
+            shortUrl: shortId,
+            url: req.body.url
+        })
+        await UrlObject.save();
+        res.send(UrlObject)
+    }catch (e){
+        console.log(e)
+    }
 });
 
 export default ShortUrlRouter;
